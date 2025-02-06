8deed3f1d1b2de5b0c80012c409f0b1b28783cad
bcook03 <bnc121203@outlook.com>
2025-02-06 08:50:42 -0600

    Added basket script and collision detection

---
+++ Assets/Scripts/ApplePicker.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplePicker : MonoBehaviour
+{
+    [Header("Inscribed")]
+    public GameObject basketPrefab;
+    public int numBaskets = 3;
+    public float basketBottomY = -14f;
+    public float basketSpacingY = 2f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        for (int i = 0; i < numBaskets; i++) {
+            GameObject tbasketGO = Instantiate<GameObject>(basketPrefab);
+            Vector3 pos = Vector3.zero;
+            pos.y = basketBottomY + (basketSpacingY * i);
+            tbasketGO.transform.position = pos;
+        }
+    }
+}
