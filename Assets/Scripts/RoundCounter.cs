4dc9cc453f342db7b161952b2f8a2ccd071e6f42
bcook03 <bnc121203@outlook.com>
2025-02-08 12:38:20 -0600

    update numBaskets, add RoundCounter

---
+++ Assets/Scripts/RoundCounter.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class RoundConrol : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public int round = 1;
+    public int maxRounds = 4;
+    
+}
