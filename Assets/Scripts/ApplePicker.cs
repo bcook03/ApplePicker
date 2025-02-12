8ebc2bec0bea5a7e8c30624c651b951649715074
bcook03 <bnc121203@outlook.com>
2025-02-11 18:18:49 -0600

    changed basket spacing to 1.5

--- Assets/Scripts/ApplePicker.cs
+++ Assets/Scripts/ApplePicker.cs
@@ -9,7 +9,7 @@ public class ApplePicker : MonoBehaviour
     public GameObject basketPrefab;
     public int numBaskets = 4;
     public float basketBottomY = -14f;
-    public float basketSpacingY = 2f;
+    public float basketSpacingY = 1.5f;
     public List<GameObject> basketList;
     public RoundCounter roundCounter;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
